d56f0d02110e826806c809efd83b8d4ce8b9a49e
BIG-Indonesia <pusatppig@gmail.com>
2020-03-27 10:28:50 +0700

    upload pertama

---
+++ cfg.py
@@ -0,0 +1,44 @@
+APP_BASE = "/opt/gspalapa-api/"
+SECURITY_TRACKABLE = True
+SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
+SECURITY_PASSWORD_SALT = 'PALAPA_ini_PALAPA_ini_PALAPA_ini_PALAPA'
+WTF_CSRF_ENABLED = False
+SECURITY_TOKEN_MAX_AGE = 365 * 24 * 60 * 60 * 1000
+SECRET_KEY = 'PALAPA ini PALAPA ini PALAPA ini PALAPA'
+SQLALCHEMY_DATABASE_URI = 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/palapa'
+SQLALCHEMY_BINDS = {
+    'dbdev': 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/palapa_dev',
+    'dbprod': 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/palapa_prod',
+    'dbpub': 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/palapa_pub',
+    'services': 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/palapa'
+}
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATASTORE = 'postgres://syspalapa:sysp4l4p4@REPGSMANAGERDOM/'
+GEOSERVER_REST_URL = 'http://REPGSMANAGERDOM:8080/geoserver/rest/'
+GEOSERVER_WMS_URL = 'http://REPGSMANAGERDOM:8080/geoserver/wms?'
+GEOSERVER_WFS_URL = 'http://REPGSMANAGERDOM:8080/geoserver/wfs?'
+GEOSERVER_WMS_OUT = 'http://REPGSMANAGERDOMOUT/geoserver/wms?'
+GEOSERVER_WFS_OUT = 'http://REPGSMANAGERDOMOUT/geoserver/wms?'
+GEOSERVER_USER = 'syspalapa'
+GEOSERVER_PASS = 'sysp4l4p4'
+GEOSERVER_THUMBNAILS = '/var/www/html/assets/thumbnails/'
+GEOSERVER_DATA_DIR = '/var/lib/tomcat/webapps/geoserver/data/'
+GEOSERVER_LAYERS_PROP = '/var/lib/tomcat/webapps/geoserver/data/security/layers.properties'
+GEOSERVER_SERVICES_PROP = '/var/lib/tomcat/webapps/geoserver/data/security/services.properties'
+DATASTORE_HOST = 'REPGSMANAGERDOM'
+DATASTORE_PORT = '5432'
+DATASTORE_USER = 'syspalapa'
+DATASTORE_PASS = 'sysp4l4p4'
+DATASTORE_DB = 'palapa'
+UPLOAD_FOLDER = '/var/palapa/uploads/'
+RASTER_FOLDER = '/var/palapa/data/'
+RASTER_STORE = '/var/palapa/data/'
+DOCUMENTS_FOLDER = '/var/palapa/documents/'
+DOWNLOADS_FOLDER = '/var/palapa/downloads/'
+ALLOWED_EXTENSIONS = set(['zip', 'ZIP','shp', 'SHP','shx', 'SHX','dbf', 'DBF','prj', 'PRJ'])
+ALLOWED_VECTOR = set(['shp', 'SHP'])
+ALLOWED_RASTER = set(['tiff', 'tif', 'TIF', 'TIFF'])
+CSW_URL = 'http://127.0.0.1/csw'
+PALAPA_FOLDER = '/var/www/html/palapa/'
+F_DOWNLOADS = '/var/www/html/assets/downloads/'
